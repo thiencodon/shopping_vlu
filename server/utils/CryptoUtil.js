43894afe314db674ba5209dc677d294b78d3ea7e
thiencodon <93118986+thiencodon@users.noreply.github.com>
2026-03-30 11:16:26 +0700

    Delete server directory

--- server/utils/CryptoUtil.js
+++
@@ -1,9 +0,0 @@
-const CryptoUtil = {
-  md5(input) {
-    const crypto = require('crypto');
-    const hash = crypto.createHash('md5').update(input).digest('hex');
-    return hash;
-  },
-};
-
-module.exports = CryptoUtil;
