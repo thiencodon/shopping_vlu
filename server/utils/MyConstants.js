5c7df2ecd36223b2648842cf6dd6969b3e029615
thiencodon <93118986+thiencodon@users.noreply.github.com>
2026-03-30 11:27:24 +0700

    server

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.cvm9ax2.mongodb.net',
+  DB_USER: 'thiendo4560',
+  DB_PASS: '0909704714',
+  DB_DATABASE: 'shoppingonline',
+  EMAIL_USER: 'thiendo4560@gmail.com',
+  EMAIL_PASS: 'xklr ajoa pays upoj',
+  JWT_SECRET: 'abciooiqwe',
+  JWT_EXPIRES: '356658435644111',
+};
+
+module.exports = MyConstants;
